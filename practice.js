76e436b630f495d36899e877184df667f9a43f58
Asif Ahmed <asifahmed331999@gmail.com>
2024-01-16 23:00:56 +0530

    add lecture 10

--- practice.js
+++ practice.js
@@ -4,15 +4,27 @@ let rollno=504
 // console.table([name,rollno]);
 // console.log(typeof name)
 
-let isloggin="0"
-let checkBoolean=Boolean(isloggin)
-console.log(typeof checkBoolean)
-console.log(checkBoolean);
+// let isloggin="0"
+// let checkBoolean=Boolean(isloggin)
+// console.log(typeof checkBoolean)
+// console.log(checkBoolean);
 
-let name01=["asif","ahmed","sunny"];
-console.log(name01);
+// let name01=["asif","ahmed","sunny"];
+// console.log(name01);
+// let myobj={
+//     name:"Asif",
+//     age:25,
+// }
+// console.table([name01,myobj]);
+let marks=[200,300,400,500,600,700];
+console.log(marks)
 let myobj={
-    name:"Asif",
-    age:25,
+    num:1001,
+    name:"Sunny"
 }
-console.table([name01,myobj]);
+
+console.log(typeof myobj);
+console.log(typeof marks);
+let logginId="Asifahmed331999@gmail.com"
+const password="Sunny@331999"
+console.table([logginId,password]);
